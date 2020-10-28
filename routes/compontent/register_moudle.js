751e2ac32d0fc7d60dadfe92023a0e60125c3d8d
silin.wang <wsl2535620700@qq.com>
2020-10-28 15:32:15 +0800

    注册

--- routes/compontent/register_moudle.js
+++ routes/compontent/register_moudle.js
@@ -12,75 +12,67 @@ connection.connect();
 // 注册：判断账号是否存在
 const is_register = (req, res, next) => {
   var data = req.body
-  if (data.userName) {
-    var sql = `SELECT * FROM \`login_information\` WHERE userName='${data.userName}'`
-    connection.query(sql, function (err, result) {
-      if(err){
-        res.json({
-          code: 500,
-          msg: err
-        })
-        return;
-      } else {
-        // state为0：判断账号是否可以注册
-        if (data.state && data.state == 0) {
-          if (result.length > 0) {
-            res.json({
-              code: 500,
-              msg: '此账号已被注册'
-            })
-          } else {
-            res.json({
-              code: 0,
-              msg: 'success'
-            })
-          }
-        } else if (data.state && data.state == 1) {
-          if (result.length > 0) {
-            if (result[0].password === data.password) {
-              if (result[0].password === data.newPassword) {
-                res.json({
-                  code: 500,
-                  msg: '新密码不能与旧密码相同'
-                })
-              } else {
-                res.json({
-                  code: 0,
-                  msg: 'success',
-                  data: result[0]
-                })
-              }
-            } else {
-              res.json({
+  var sql = `SELECT * FROM \`login_information\` WHERE name='${req.body.name}'`
+  connection.query(sql, function (err, result) {
+    if (!err) {
+      res.end(JSON.stringify({
+        code: 500,
+        msg: err
+      }))
+      return;
+    } else {
+      // state为0：注册，state为1：修改密码
+      if (data.state && data.state === '0') {
+        if (result.length > 0) {
+          res.end(JSON.stringify({
+            code: 500,
+            msg: '此账号已被注册'
+          }))
+        } else {
+          res.end(JSON.stringify({
+            code: 0,
+            msg: 'success'
+          }))
+        }
+      } else if (data.state && data.state === '1') {
+        if (result.length > 0) {
+          if (result[0].password === data.password) {
+            if (result[0].password === data.newPassword) {
+              res.end(JSON.stringify({
                 code: 500,
-                msg: '密码错误'
-              })
+                msg: '新密码不能与旧密码相同'
+              }))
+            } else {
+              res.end(JSON.stringify({
+                code: 0,
+                msg: 'success',
+                data: result[0]
+              }))
             }
           } else {
-            res.json({
+            res.end(JSON.stringify({
               code: 500,
-              msg: '用户名输入错误'
-            })
+              msg: '密码错误'
+            }))
           }
         } else {
-          res.json({
+          res.end(JSON.stringify({
             code: 500,
-            msg: '未知异常，请联系管理员'
-          })
+            msg: '用户名输入错误'
+          }))
         }
+      } else {
+        res.end(JSON.stringify({
+          code: 500,
+          msg: '未知异常，请联系管理员'
+        }))
       }
-    });
-  } else {
-    res.json({
-      code: 500,
-      msg: '未知异常，请联系管理员'
-    })
-  }
+    }
+  });
 }
 // 注册：将注册信息写入数据库
 const register_inster = (req, res, next) => {
   console.log(req.body)
-  // var addSql = 'INSERT INTO login_information(Id,userName,password,number) VALUES(0,?,?,0)';
   var addSql = 'INSERT INTO `login_information` (`id`, `userName`, `password`, `name`, `gender`, `age`, `emotional`, `height`, `weight`, `nation`, `address`, `school`, `hobby`, `occupation`, `personalSignature`) VALUES (NULL, ?, ?, \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\', \'\')';
   var addSqlParams = [req.body.userName, req.body.password];
   connection.query(addSql, addSqlParams, function (err, result) {
