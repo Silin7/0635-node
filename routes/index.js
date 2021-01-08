a00400b6dd596b50af0e1d23ca7d8428142952fe
silin.wang <wsl2535620700@qq.com>
2021-01-08 10:12:14 +0800

    module

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 // 权限模块
-const adminMoudle = require('./compontent/admin_moudle')
+const adminMoudle = require('./compontent/admin_module')
 router.get('/admin/admin_news_type', adminMoudle.admin_news_type);
 router.get('/admin/admin_city_type', adminMoudle.admin_city_type);
 
 
 // 注册，登录模块
-const loginMoudle = require('./compontent/login_moudle')
+const loginMoudle = require('./compontent/login_module')
 router.get('/login/is_register', loginMoudle.is_register);
 router.post('/login/register_inster', loginMoudle.register_inster);
 router.post('/login/change_password', loginMoudle.change_password);
 router.post('/login/sign_in', loginMoudle.sign_in);
 
 // 个人中心模块
-const mineMoudle = require('./compontent/mine_moudle')
+const mineMoudle = require('./compontent/mine_module')
 router.get('/mine/mine_info', mineMoudle.mine_info);
 router.post('/mine/update_mineInfo', mineMoudle.update_mineInfo);
 router.get('/mine/concerns_list', mineMoudle.concerns_list);
@@ -24,7 +24,7 @@ router.post('/mine/follow_users', mineMoudle.follow_users);
 router.post('/mine/cancel_users', mineMoudle.cancel_users);
 
 // 话题模块
-const conversationMoudle = require('./compontent/conversation_moudle')
+const conversationMoudle = require('./compontent/conversation_module')
 router.get('/conversation/conversation_list', conversationMoudle.conversation_list);
 router.get('/conversation/conversation_info', conversationMoudle.conversation_info);
 router.get('/conversation/mine_conversation_list', conversationMoudle.mine_conversation_list);
@@ -33,7 +33,7 @@ router.post('/conversation/follow_conversation', conversationMoudle.follow_conve
 router.post('/conversation/cancel_conversation', conversationMoudle.cancel_conversation);
 
 // 景点模块
-const scenicspotMoudle = require('./compontent/scenicspot_moudle')
+const scenicspotMoudle = require('./compontent/scenicspot_module')
 router.get('/scenicspot/scenicspot_list', scenicspotMoudle.scenicspot_list);
 router.get('/scenicspot/scenicspot_info', scenicspotMoudle.scenicspot_info);
 router.get('/scenicspot/mine_scenicspot_list', scenicspotMoudle.mine_scenicspot_list);
@@ -46,20 +46,20 @@ const historyModule = require('./compontent/history_module')
 router.get('/history/historical_evolution', historyModule.historical_evolution);
 
 // 菜单模块
-const recipeMoudle = require('./compontent/recipe_moudle')
+const recipeMoudle = require('./compontent/recipe_module')
 router.get('/recipe/recipe_catalogs', recipeMoudle.recipe_catalogs);
 router.get('/recipe/recipe_list', recipeMoudle.recipe_list);
 router.get('/recipe/recipe_detail', recipeMoudle.recipe_detail);
 
 // 消息模块
-const messageModules = require('./compontent/message_moudle')
+const messageModules = require('./compontent/message_module')
 router.get('/message/permessage_list', messageModules.permessage_list);
 router.get('/message/permessage_details', messageModules.permessage_details);
 router.get('/message/sysmessage_list', messageModules.sysmessage_list);
 router.get('/message/sysmessage_details', messageModules.sysmessage_details);
 
 // 动态模块
-const dynamicModules = require('./compontent/dynamic_moudle')
+const dynamicModules = require('./compontent/dynamic_module')
 router.get('/dynamic/dynamic_news_list', dynamicModules.dynamic_news_list);
 router.get('/dynamic/dynamic_news_details', dynamicModules.dynamic_news_details);
 router.get('/dynamic/dynamic_list', dynamicModules.dynamic_list);
