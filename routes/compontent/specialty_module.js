4db7f9e89f3e9d9c6a9cc2bc0039a6890cf07c36
silin.wang <wsl2535620700@qq.com>
2021-02-01 14:24:07 +0800

    特产  分区  分页

--- routes/compontent/specialty_module.js
+++ routes/compontent/specialty_module.js
@@ -2,18 +2,42 @@ const conn = require('./mySQL')
 
 // 特产列表
 const specialty_list = (req, res, next) => {
-  let sql = `SELECT id, name, image FROM \`local_specialty\` WHERE 1`
-  conn().query(sql, function (err, result) {
-    if(err){
+  let data = req.query
+  let slimit = (data.page - 1) * data.limit
+  let elimit = (data.page) * data.limit
+  let sql1 = 'SELECT COUNT(*) FROM `local_specialty`'
+  let sql2 = `SELECT id, specialty_name, specialty_cover, specialty_position FROM \`local_specialty\` WHERE \`specialty_show\` = '01'`
+  let specialty_position = ` AND \`specialty_position\` = '${data.specialty_position}'`
+  let scenicspot_limit = ` LIMIT ${slimit},${elimit}`
+  if (data.specialty_position && data.specialty_position !== '') {
+    sql2 = sql2 + specialty_position + scenicspot_limit
+  } else {
+    sql2 = sql2 + scenicspot_limit
+  }
+  conn().query(sql1, function (err1, result1) {
+    if(err1){
       res.json({
         code: 500,
-        msg: err
+        msg: err1
       })
     } else {
-      res.json({
-        code: 0,
-        msg: 'success',
-        data: result
+      let totalCount = result1[0][`COUNT(*)`]
+      conn().query(sql2, function (err2, result2) {
+        if(err2){
+          res.json({
+            code: 500,
+            msg: err2
+          })
+        } else {
+          res.json({
+            code: 0,
+            msg: 'success',
+            page: data.page,
+            limit: data.limit,
+            totalCount: totalCount,
+            data: result2
+          })
+        }
       })
     }
   })
