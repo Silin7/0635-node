fd1899ab9d5976019f8f8d4eb209c9de77c40064
silin.wang <wsl2535620700@qq.com>
2021-08-15 12:45:59 +0800

    config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
  * @Author: silin7
  * @Date: 2021-08-15
  */
+
 const path = require('path');
 
 module.exports = {
   Port: 3000, // 启动端口
   staticDir: path.resolve('./public'), // 静态资源路径
   uploadDir: path.join(__dirname, path.resolve('public/')), // 上传文件路径
-  // 数据库连接设置
-  dbConfig: {
+  dbConfig: { // 数据库连接设置
     connectionLimit: 10,
     host: 'localhost',
     user: 'root',
